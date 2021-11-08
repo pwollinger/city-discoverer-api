077d432a7839ec8dfaafbc7a6fd21e342135ed1b
Pedro Alexandre Wollinger <pwollinger02@gmail.com>
2021-11-07 22:12:52 -0300

    Added endpoint ‘city’ that for now only returns the temperature for the city informed as the parameter city_name.

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from fastapi import FastAPI
-from pyowm.owm import OWM
+#from pyowm.owm import OWM
+import pyowm
 import os
+
+app = FastAPI()
+owm = pyowm.owm.OWM(os.environ['OW_API_KEY'])
+mgr = owm.weather_manager()
+
+@app.get("/city/")
+def get_city_info(city_name: str):
+    city_name = city_name.replace("_", " ")
+    try:
+        location_data = mgr.weather_at_place(city_name)
+        wheather_data = location_data.weather
+        return wheather_data.temperature(unit='celsius')
+    except pyowm.commons.exceptions.NotFoundError:
+        return {"Error": "City not found!"}
